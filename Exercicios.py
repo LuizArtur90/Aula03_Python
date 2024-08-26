3dfee9792ebac0abcde008813118269d1221a6cc
Luiz Artur <luiz.lamoglia@gmail.com>
2024-08-26 19:26:31 -0300

    subindo exercicios

---
+++ Exercicios.py
@@ -0,0 +1,33 @@
+#Exercício 2: Classificação de Dados de Sensor
+#Imagine que você está trabalhando com dados de sensores IoT. Os dados incluem medições de temperatura. Você precisa classificar cada leitura como 'Baixa', 'Normal' ou 'Alta'. Considerando que:
+#Temperatura < 18°C é 'Baixa'
+#Temperatura >= 18°C e <= 26°C é 'Normal'
+#Temperatura > 26°C é 'Alta'
+
+#temp = float(input("Entre com o valor da temperatura atual:"))
+#temp = 20
+#if temp < 18:
+#    print("Temperatura baixa!")
+#elif 18<= temp <=26:
+#    print("Temperatura normal.")
+#else:
+#    print("Temperatura Alta!")
+
+
+
+#Exercício 4: Validação de Dados de Entrada
+#Antes de processar os dados de usuários em um sistema de recomendação, você precisa garantir que cada usuário tenha idade entre 18 e 65 anos e tenha fornecido um email válido. Escreva um programa que valide essas condições e imprima "Dados de usuário válidos" ou o erro específico encontrado.
+
+#idade = int(input("Entre com a idade do usuário:"))
+#email = str(input("Entre com o e-mail do usuário:"))
+#if 18<=idade<=65:
+#    print("Idade não permitida para cadastro, apenas usuários entre 18 e 65 anos.")
+#elif "@" not in email or "." not in email:
+#    print("Email não válido, favor verificar.")
+#else:
+#    print("Cadastro realizado com sucesso!")
+
+
+
+
+
